8980044e836ea891245518341a6d8f54cc3e93f2
one-zeze <tales0708@gmail.com>
2024-04-19 21:45:16 +0900

    project init

---
+++ src/main/java/onezeze/diginom/DiginomApplication.java
@@ -0,0 +1,13 @@
+package onezeze.diginom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiginomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiginomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onezeze/diginom/DiginomApplicationTests.java
@@ -0,0 +1,13 @@
+package onezeze.diginom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiginomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
